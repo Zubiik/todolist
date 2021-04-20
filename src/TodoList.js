d5e504d82d2ad2aaa7e97f3a00092c621cb56123
Zubiik <elodiefrappat08@gmail.com>
2021-04-20 11:25:23 -0400

    fix namejs

---
+++ src/TodoList.js
@@ -0,0 +1,23 @@
+import {React,useState} from 'react';
+
+
+export default function TodoList() {
+
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const todoTitleInput = (setTodoStorage) => {
+
+  }
+
+  return (
+    <div>
+    <input >
+    </input>
+    <textarea>
+
+    </textarea>
+    <button>add</button>
+    </div>
+  );
+}
+
