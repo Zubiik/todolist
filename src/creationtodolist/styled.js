c1f3b1a41a212a4dd5c546fb759c77e3bfb280e7
Zubiik <elodiefrappat08@gmail.com>
2021-04-28 19:20:46 -0400

    fix : styled component

--- src/creationtodolist/styled.js
+++ src/creationtodolist/styled.js
@@ -5,7 +5,6 @@ margin:10px;
 display:flex;
 background-color:${({theme}) => theme.color.grey};
 border-radius:5px;
-border: solid 1px orange;
 width:250px;
 min-height:150px;
 flex-direction:column;
@@ -15,22 +14,19 @@ position:relative;
 `;
 
 export const BodyCustom = styled.div `
-border: solid 1px pink;
 display:flex;
 border-radius:5px;
 `;
 
 export const InputCustomTitle = styled.div`
-border: solid 1px pink;
 background-color:${({theme}) => theme.color.white};
-margin:10px;
+margin:5px;
+padding:10px;
 width:80%;
 border-radius:5px;
 `;
 
 export const InputCustomText = styled(InputCustomTitle)`
-border: solid 1px red;
-hyphens: auto;
 `;
 
 
@@ -74,17 +70,15 @@ height:40px;
 `;
 
 export const ChangeStatutButton = styled(AddButton)`
-border: solid 1px blue;
 `;
 
-export const RemoveButton = styled(AddButton)`
+export const RemoveButtonCustom = styled(AddButton)`
 background-color:${({theme}) => theme.color.orange};
 position:absolute;
 top:5px; right:5px;
 `;
 
 export const ContainerChangeStatutButton = styled.div`
-border: solid 1px yellow;
 display:flex;
 justify-content:space-around;
 `;
--- src/statutChanges/styled.js
+++ src/statutChanges/styled.js
@@ -1,38 +1,56 @@
 import styled from '@emotion/styled'
 
 export const BodyCustom = styled.div`
-min-width:30%;
-background-color:${({theme}) => theme.color.grey};
+width:250px;
 display:flex;
 flex-direction:column;
 margin:5px;
 padding:5px;
-border-radius:5px;
+border-radius:5px; 
+position: relative;
 `;
 export const TaskCustom = styled.div`
 margin:10px;
+margin-left:-1px;
 display:flex;
 width:250px;
 min-height:150px;
-background-color:${({theme}) => theme.color.white};
+background-color:${({theme}) => theme.color.grey};
 border-radius:5px;
-border: solid 1px orange;
+border: solid 1px white bold;
 flex-direction:column;
 justify-content:space-around;
-box-shadow: 10px 5px 5px grey;
+box-shadow: 5px 2px 2px grey;
+position:relative;
+
 `;
 
 export const TitleCustom = styled.div`
-color:${({theme}) => theme.color.black};
-
+color:${({theme}) => theme.color.white};
+font-size:20px;
+display:flex;
+justify-content:center;
 `;
 
 export const ChangeStatutButton = styled.div`
 display:flex;
 justify-content:center;
-background-color:${({theme}) => theme.color.lightBlue};
+margin:5px;
+background-color:${({theme}) => theme.color.orange};
 color:${({theme}) => theme.color.white};
 border-radius:5px;
 padding:5px;
 box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.22);
+
 `;
+
+export const InputCustomTitle = styled.div`
+background-color:${({theme}) => theme.color.white};
+margin:5px;
+padding:10px;
+width:80%;
+border-radius:5px;
+`;
+export const InputCustomText = styled(InputCustomTitle)`
+`;
+
