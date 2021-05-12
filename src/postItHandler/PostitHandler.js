a7d0e756942d4eda4f7e359ad6dbbf073c8ecc49
Zubiik <elodiefrappat08@gmail.com>
2021-05-12 09:27:38 -0400

    fix alignement

---
+++ src/postItHandler/PostitHandler.js
@@ -0,0 +1,57 @@
+import { React, useState } from "react";
+import UrgentZone from "../statutChanges/UrgentZone";
+import NonUrgentZone from "../statutChanges/NonUrgentZone";
+import { Testing } from "./styled";
+import Postit from "../creationtodolist/Postit";
+
+export default function TodoList() {
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoTask, setTodoTask] = useState("");
+  const [storage, setStorage] = useState([]);
+  const [editTask, setEditTask] = useState(false);
+  const todoInput = (event, saveText) => {
+    saveText(event.target.value);
+  };
+
+  const urgentButton = (index) => {
+    var storageCopy = storage;
+    storageCopy[index].statut = "urgent";
+    setStorage([...storageCopy]);
+  };
+
+  const nonUrgentButton = (index) => {
+    var storageCopy = storage;
+    storageCopy[index].statut = "nonUrgent";
+    setStorage([...storageCopy]);
+  };
+
+  const addButton = () => {
+    setStorage([
+      ...storage,
+      { statut: "default", title: todoTitle, text: todoTask },
+    ]);
+    setTodoTitle("");
+    setTodoTask("");
+  };
+
+  return (
+    <Testing>
+      <Postit
+        addButton={addButton}
+        nonUrgentButton={nonUrgentButton}
+        urgentButton={urgentButton}
+        todoInput={todoInput}
+        todoTitle={todoTitle}
+        setTodoTitle={setTodoTitle}
+        todoTask={todoTask}
+        setTodoTask={setTodoTask}
+        storage={storage}
+        setStorage={setStorage}
+        editTask={editTask}
+        setEditTask={setEditTask}
+      />
+      <UrgentZone storage={storage} setStorage={setStorage} />
+      <NonUrgentZone storage={storage} setStorage={setStorage} />
+    </Testing>
+  );
+}
---
+++ src/postItHandler/styled.js
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const Testing = styled.div`
+  border: 1px solid pink;
+  display: flex;
+`;
